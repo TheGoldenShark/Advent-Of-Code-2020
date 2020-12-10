4bc0fa06d4624dfb5ecebdf22fb70bbde0ab1e21
Tom <whitethomas136@gmail.com>
2020-12-10 19:02:50 +0000

    Day 6 Complete

--- D6/D6.5.py
+++ D6/D6.5.py
@@ -1,13 +1,15 @@
+import string
+
 with open("input.txt","r") as f: data=f.readlines()
 data = [x[:-1] for x in data]
 dataProcessed = []
-temp=set()
+temp=set(string.ascii_lowercase)
 for i in data:
     if i== '':
         dataProcessed.append(temp)
-        temp = set()
+        temp=set(string.ascii_lowercase)
     else:
-        temp = temp.union(set(i))
+        temp = temp.intersection(set(i))
 dataProcessed.append(temp)
 
 count = 0
---
+++ D6/test.py
@@ -0,0 +1,3 @@
+arr = "ansdkog"
+arr = set(arr)
+print()
